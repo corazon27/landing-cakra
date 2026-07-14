322b95dd193551012da0ad9dba1ddcb2f70e03b4
corazon27 <muhalfian1899@gmail.com>
2026-07-14 20:05:55 +0700

    perbaikan welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1199,7 +1199,6 @@
 
         const createSwiper = () => {
             new Swiper('.testimonial-swiper', {
-            modules: [SwiperModules.Navigation, SwiperModules.Pagination],
                 slidesPerView: 1,
                 spaceBetween: 20,
                 loop: true,
